5c5e5862fc3870532071bc3711a28fb4237fe757
Liam Grossman <liam.j.grossman@gmail.com>
2026-04-14 21:15:46 -0400

    feat(web): add settings button on hover (#18)

--- packages/web/src/lib/components/common/Workspace.svelte
+++ packages/web/src/lib/components/common/Workspace.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import type { Workspace } from "$lib/repository/workspaceRepository";
+	import { Settings2 } from "@lucide/svelte";
+	import Icon from "./Icon.svelte";
 
   interface Props {
     workspace: Workspace;
@@ -9,11 +11,23 @@
 </script>
 
 <div
-  class="p-5 hover:bg-gray-100/30 transition ease-in-out"
+  class="p-5 flex flex-row justify-between items-center hover:bg-gray-100/30 transition ease-in-out group"
 >
   <a href={`/${workspace.name}`} class="flex-1 block">
     <h2 class="font-medium text-black w-full">
       {workspace.name}
     </h2>
   </a>
+   <div
+    class="group-hover:opacity-100 group-hover:scale-100 scale-99 opacity-0 ease-in-out transition"
+  >
+    <a href={`/${workspace.name}/settings`}>
+      <button
+        class="text-sm cursor-pointer flex font-medium px-3 items-center h-7 space-x-2 text-black border border-gray-300 rounded-full shadow-sm shadow-gray-100"
+      >
+        <Icon icon={Settings2} size={15} />
+        <span>Settings</span>
+      </button>
+    </a>
+  </div>
 </div>
