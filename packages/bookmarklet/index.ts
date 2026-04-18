bdd167cd29c30204d08ca0c5538ec90c88672e77
Liam Grossman <liam.j.grossman@gmail.com>
2026-04-18 09:04:53 -0400

    feat(web): bookmarklet create (#51)
    * feat(web): create bookmarklet with scrape lib
    
    * feat(web): add bookmarklet build in static
    
    * feat(bookmarklet): create build process
    
    * feat(bookmarklet): include current url

--- packages/bookmarklet/index.ts
+++ packages/bookmarklet/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { scrape } from "@barque/scrape";
+
+(async () => {
+  const content = document.documentElement.outerHTML;
+  const text = await scrape.DOMParser(content);
+  const encodedText = encodeURIComponent(text);
+  const currentUrl = window.location.href;
+  window.location.href = `http://localhost:7009?text=${encodedText}&url=${currentUrl}`;
+})();
