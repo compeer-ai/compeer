b28c0cc6a4fb153cdecfc1ab7f49f6d0d30ca9d0
Liam Grossman <liam.j.grossman@gmail.com>
2026-04-26 19:43:09 -0400

    chore: clean up dead code (#85)

--- packages/web/src/lib/models/dotPaths.ts
+++
@@ -1,7 +0,0 @@
-export type DotPaths<T, Prefix extends string = ''> = {
-	[K in keyof T]: K extends string
-		? T[K] extends Record<string, any>
-			? `${Prefix}${K}` | DotPaths<T[K], `${Prefix}${K}.`>
-			: `${Prefix}${K}`
-		: never;
-}[keyof T];
