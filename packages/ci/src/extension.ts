a494189238af59efd3a55a81d96ed50d8edc4df3
Liam Grossman <liam.j.grossman@gmail.com>
2026-04-26 19:33:08 -0400

    chore(ci): clean up

--- packages/ci/src/extension.ts
+++
@@ -1,8 +0,0 @@
-import path from "node:path";
-import { $ } from "bun";
-
-const ROOT_DIR = path.resolve(import.meta.dir, "../../..");
-const CLI_DIR = path.resolve(ROOT_DIR, "packages", "extension");
-
-await $`bun run zip`.cwd(CLI_DIR);
-await $`bun run zip:firefox`.cwd(CLI_DIR);
