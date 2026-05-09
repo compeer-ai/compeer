2cdec19373e62f99bd6669586ee308a31b43f8c5
Liam Grossman <liam.j.grossman@gmail.com>
2026-05-09 12:28:26 -0400

    chore(web): move to hono-openapi

--- packages/web/src/lib/repository/workspaceRepository.ts
+++ packages/web/src/lib/repository/workspaceRepository.ts
@@ -1,8 +1,10 @@
 import { type InferSelectModel } from 'drizzle-orm';
 import { BaseRepoistory } from './baseRepository';
 import { workspaceTable } from '$lib/utilities/schema';
+import { createSelectSchema } from 'drizzle-valibot';
 
 export type Workspace = InferSelectModel<typeof workspaceTable>;
+export const workspaceSchema = createSelectSchema(workspaceTable);
 
 export class WorkspaceRepository extends BaseRepoistory<typeof workspaceTable, string> {
 	constructor() {
