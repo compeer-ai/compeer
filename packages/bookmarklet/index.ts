e201cc9061addd878f3b4d46fb4487503dea5f65
Liam Grossman <liam.j.grossman@gmail.com>
2026-05-09 10:25:38 -0400

    feat(bookmarklet): support text capture selection (#115)

--- packages/bookmarklet/index.ts
+++ packages/bookmarklet/index.ts
@@ -1,9 +1,28 @@
 import { scrape } from "@compeer-ai/scrape";
 
 (async () => {
-  const content = document.documentElement.outerHTML;
-  const text = await scrape.DOMParser(content);
-  const encodedText = encodeURIComponent(text);
-  const currentUrl = window.location.href;
-  window.location.href = `http://localhost:7009?text=${encodedText}&url=${currentUrl}`;
+  const selection = window.getSelection();
+  let selectedContent = "";
+  let urlCapture = false;
+  if (selection && selection.toString().length > 0) {
+    selectedContent = selection.toString();
+  } else if (selection && selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    const fragment = range.cloneContents();
+    const tempDiv = document.createElement("div");
+    tempDiv.appendChild(fragment);
+    selectedContent = tempDiv.innerHTML;
+  } else {
+    const content = document.documentElement.outerHTML;
+    selectedContent = await scrape.DOMParser(content);
+    urlCapture = true;
+  }
+  const encodedText = encodeURIComponent(selectedContent);
+  const url = new URL("http://localhost:3000")
+  if (urlCapture) {
+    const currentUrl = window.location.href;
+    url.searchParams.set('url', currentUrl);
+  }
+  url.searchParams.set('text', encodedText);
+  window.location.href = url.toString();
 })();
