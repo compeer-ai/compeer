a76f263e1fcf08f6abe832312b5180ec72f7a6ff
Liam Grossman <liam.j.grossman@gmail.com>
2026-05-15 17:07:55 -0400

    feat(ai-sdk): create

--- packages/ai-sdk/index.ts
+++ packages/ai-sdk/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
+
+export function createCompeerProvider(args: { url: string, workspace: string, store?: string, apiKey?: string, bearer?: string, headers: Record<string, string> }) {
+    const { url , workspace, store, apiKey, bearer, headers: inferenceHeaders } = args;
+    let headers: Record<string, string> = inferenceHeaders
+    if (apiKey) {
+        headers['X-Api-Key'] = apiKey;
+    } else if (bearer) {
+        headers['Authorization'] = `Bearer ${bearer}`
+    }
+    const baseURL = store ? new URL(`/${workspace}/${store}/inference`, url) : new URL(`/${workspace}/inference`, url)
+    return createOpenAICompatible({
+        baseURL: baseURL.toString(),
+        name: "compeer",
+        headers
+    })
+}
