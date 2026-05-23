65070da3273fae90d3098f8b0bf0f8bf9aa2af59
Liam Grossman <liam.j.grossman@gmail.com>
2026-05-23 09:05:45 -0400

    chore(ci): deprecate (#167)

--- packages/ci/src/cli.ts
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import { $ } from "bun";
-
-const ROOT_DIR = path.resolve(import.meta.dir, "../../..");
-const CLI_DIR = path.resolve(ROOT_DIR, "packages", "cli");
-const OTP = process.argv[2];
-
-await $`bun run build`.cwd(CLI_DIR);
-await $`npm publish --access public --otp ${OTP}`.cwd(CLI_DIR).quiet();
