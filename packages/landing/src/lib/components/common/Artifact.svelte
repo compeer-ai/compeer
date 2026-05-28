3e0769ee862249ddc5cb170602bab87480c5ef27
Liam Grossman <liam.j.grossman@gmail.com>
2026-05-27 22:23:32 -0400

    feat(landing): create marketplace UI (#178)

---
+++ packages/landing/src/lib/components/common/Artifact.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+    interface Props {
+        title: string
+        description: string;
+        tags: string[]
+    }
+    const { title, description, tags }: Props = $props();
+</script>
+
+<div class="border border-gray-300 shadow-sm shadow-gray-100 bg-white rounded-lg space-y-3">
+    <div class="space-y-2">
+        <h2 class="text-lg font-medium text-black">{title}</h2>
+        <p>{description}</p>
+    </div>
+    <div class="flex space-x-2">
+        {#each tags as tag}
+            <div class="h-7 px-3 rounded-full border border-gray-300">
+                {tag}
+            </div>
+        {/each}
+    </div>
+</div>
