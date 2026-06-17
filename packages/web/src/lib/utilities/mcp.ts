9fe08820bc484d94c6ee1b7e6953565f1721196d
Liam Grossman <liam.j.grossman@gmail.com>
2026-06-16 21:38:13 -0400

    feat(web): add mcp scaffolding (#221)

---
+++ packages/web/src/lib/utilities/mcp.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono';
+import { validator } from 'hono-openapi';
+import * as v from 'valibot';
+import { HttpTransport } from '@tmcp/transport-http';
+import { McpServer } from 'tmcp';
+import { ValibotJsonSchemaAdapter } from '@tmcp/adapter-valibot';
+import pkg from '../../../package.json';
+import { errors } from './errors';
+
+const paramsSchema = v.object({
+	workspace: v.string(),
+	store: v.string()
+});
+
+const router = new Hono().post('/*', validator('param', paramsSchema), async (c) => {
+	const { workspace, store } = c.req.valid('param');
+	const mcpServer = new McpServer(
+		{
+			name: store ? `${workspace}-${store}-server` : `${workspace}-server`,
+			version: pkg.version
+		},
+		{
+			adapter: new ValibotJsonSchemaAdapter(),
+			instructions: `Use this server for finding context within ${workspace}${store ? `/${store}` : ''}`
+		}
+	);
+
+	const transport = new HttpTransport(mcpServer);
+	const response = await transport.respond(c.req.raw);
+	if (!response) return c.status(500);
+	return response;
+});
