b48e9f6ee1c2d502de4e0f5e968cf054b0b1e347
Liam Grossman <liam.j.grossman@gmail.com>
2026-06-24 22:16:23 -0400

    feat(web): mount MCP server

--- packages/web/src/lib/utilities/mcpServer.ts
+++ packages/web/src/lib/utilities/mcpServer.ts
@@ -9,14 +9,14 @@ import { readSearchCaptures } from '$lib/remotes/capture.remote';
 import defaultConfiguration from '../assets/defaultConfig.json';
 import configuration from '../assets/config.json';
 
-export const API_KEYS = configuration.apiKeys || defaultConfiguration.apiKeys;
+const API_KEYS = configuration.apiKeys || defaultConfiguration.apiKeys;
 
 const paramsSchema = v.object({
 	workspace: v.string(),
 	store: v.string()
 });
 
-export const router = new Hono()
+export const mcpServer = new Hono()
 	.use(async (c, next) => {
 		if (!API_KEYS.length) return next();
 		const apiKey = c.req.header('X-Api-Key');
@@ -25,16 +25,73 @@ export const router = new Hono()
 		}
 		return c.status(401);
 	})
-	.post('/*', validator('param', paramsSchema), async (c) => {
+	.post(
+		'/workspace/:workspace/mcp',
+		validator(
+			'param',
+			v.object({
+				workspace: v.string()
+			})
+		),
+		async (c) => {
+			const { workspace } = c.req.valid('param');
+			const mcpServer = new McpServer(
+				{
+					name: `${workspace}-server`,
+					version: pkg.version
+				},
+				{
+					adapter: new ValibotJsonSchemaAdapter(),
+					instructions: `Use this server for finding context within "${workspace}"`
+				}
+			);
+			mcpServer.tool(
+				{
+					name: 'search',
+					description: 'Search for context that might be useful',
+					schema: v.object({
+						query: v.string()
+					}),
+					outputSchema: v.object({
+						result: v.array(
+							v.object({
+								content: v.string()
+							})
+						)
+					})
+				},
+				async ({ query }) => {
+					const result = await readSearchCaptures({ workspace, query });
+					return {
+						content: [
+							{
+								type: 'text',
+								text: JSON.stringify(result)
+							}
+						],
+						structuredContent: {
+							result
+						}
+					};
+				}
+			);
+
+			const transport = new HttpTransport(mcpServer);
+			const response = await transport.respond(c.req.raw);
+			if (!response) return c.status(500);
+			return response;
+		}
+	)
+	.post('/workspace/:workspace/store/:store/mcp', validator('param', paramsSchema), async (c) => {
 		const { workspace, store } = c.req.valid('param');
 		const mcpServer = new McpServer(
 			{
-				name: store ? `${workspace}-${store}-server` : `${workspace}-server`,
+				name: `${workspace}-${store}-server`,
 				version: pkg.version
 			},
 			{
 				adapter: new ValibotJsonSchemaAdapter(),
-				instructions: `Use this server for finding context within ${workspace}${store ? `/${store}` : ''}`
+				instructions: `Use this server for finding context within "${workspace}/${store}"`
 			}
 		);
 		mcpServer.tool(
