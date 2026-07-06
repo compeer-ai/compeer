02eea4c3772b2da80077d9e4b6b0b0b6a498420b
Liam Grossman <liam.j.grossman@gmail.com>
2026-07-06 17:52:17 -0400

    feat(web): unify routers to a `+server.ts` (#253)
    * feat(web): unify routers
    
    * feat(web): create client for remotes backend

---
+++ packages/web/src/lib/utilities/remotes.ts
@@ -0,0 +1,20 @@
+import { Hono } from 'hono';
+import { captureRouter } from '$lib/routers/captureRouter';
+import { configRouter } from '$lib/routers/configRouter';
+import { integrationRouter } from '$lib/routers/integrationRouter';
+import { jwtRouter } from '$lib/routers/jwtRouter';
+import { storeRouter } from '$lib/routers/storeRouter';
+import { userRouter } from '$lib/routers/userRouter';
+import { workspaceRouter } from '$lib/routers/workspaceRouter';
+
+export const remotes = new Hono()
+	.route('/user', userRouter)
+	.route('/jwt', jwtRouter)
+	.route('/config', configRouter)
+	.route('/capture', captureRouter)
+	.route('/store', storeRouter)
+	.route('/workspace', workspaceRouter)
+	.route('/integration', integrationRouter);
+
+export type RemotesRouter = typeof remotes;
+export type RemotesAppType = typeof remotes;
