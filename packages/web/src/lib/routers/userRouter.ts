cbd36e9529fe0afce7208c9b163046fb3d36ac69
Liam Grossman <liam.j.grossman@gmail.com>
2026-07-11 21:52:27 -0400

    feat(web): extract schemas for user router (#261)

--- packages/web/src/lib/routers/userRouter.ts
+++ packages/web/src/lib/routers/userRouter.ts
@@ -1,9 +1,13 @@
 import type { User } from '$lib/models/user';
 import { Hono } from 'hono';
 import { getCookie } from 'hono/cookie';
+import { validator } from 'hono-openapi';
+import * as v from 'valibot';
+
+export const readUserParamsSchema = v.object({});
 
 export const userRouter = new Hono()
-    .get('read_user', async (ctx) => {
+    .get('read_user', validator('param', readUserParamsSchema), async (ctx) => {
         const user = getCookie(ctx, 'user');
         if (user) {
             return ctx.json<User>(JSON.parse(user))
