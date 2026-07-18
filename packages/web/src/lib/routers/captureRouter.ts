73cbc753aec058d104205f8efff4645ba8e633b4
Liam Grossman <liam.j.grossman@gmail.com>
2026-07-17 21:11:15 -0400

    feat(web): implement rpc (#271)

--- packages/web/src/lib/routers/captureRouter.ts
+++ packages/web/src/lib/routers/captureRouter.ts
@@ -8,6 +8,7 @@ import { and, eq, inArray } from 'drizzle-orm';
 import { captureChunkTable, captureTable } from '$lib/utilities/schema';
 import { scrape } from '@compeer-ai/scrape';
 import { db } from '$lib/utilities/sqlite';
+import { rpc } from '$lib/utilities/rpc';
 
 const captureRepository = new CaptureRepository();
 const Type = {
@@ -16,16 +17,6 @@ const Type = {
 	url: 'url'
 } as const;
 
-export const readSearchCapturesParamsSchema = v.object({
-	store: v.optional(v.string()),
-	workspace: v.string(),
-	query: v.string()
-});
-export const readCapturesParamsSchema = v.object({
-	storeId: v.string(),
-	offset: v.optional(v.number()),
-	limit: v.optional(v.number())
-});
 export const createCaptureSchema = v.object({
 	type: v.enum(Type),
 	content: v.string(),
@@ -57,174 +48,165 @@ export const deleteCapturesSchema = v.object({
 	storeId: v.string()
 });
 
+const readCapturesRpc = await rpc.query(
+	'read_captures',
+	v.object({
+		storeId: v.string(),
+		offset: v.optional(v.number()),
+		limit: v.optional(v.number())
+	}),
+	async (args) => {
+		const result = await captureRepository.readByPredicate(
+			eq(captureTable.storeId, args.storeId),
+			args.offset,
+			args.limit
+		);
+		const captures = result.all();
+		const formattedCaptures = captures.map(({ embedding, ...others }) => others);
+		return formattedCaptures;
+	}
+);
+
+const readSearchCapturesRpc = await rpc.query(
+	'read_search_captures',
+	v.object({
+		store: v.optional(v.string()),
+		workspace: v.string(),
+		query: v.string()
+	}),
+	async (args) => {
+		const { query, store, workspace } = args;
+		const context = store
+			? await rag.projectContext(query, workspace, store)
+			: await rag.mainContext(query, workspace);
+		return context;
+	}
+);
+
 export const captureRouter = new Hono()
 	.get(
 		'read_search_captures',
-		validator('param', readSearchCapturesParamsSchema),
-		async (ctx) => {
-			const { query, store, workspace } = ctx.req.valid('param');
-			const context = store
-				? await rag.projectContext(query, workspace, store)
-				: await rag.mainContext(query, workspace);
-			return ctx.json(context);
-		}
+		readSearchCapturesRpc.validator,
+		readSearchCapturesRpc.handler
 	)
-	.get(
-		'read_captures',
-		validator('param', readCapturesParamsSchema),
-		async (ctx) => {
-			const validatedPayload = ctx.req.valid('param');
-			const result = await captureRepository.readByPredicate(
-				eq(captureTable.storeId, validatedPayload.storeId),
-				validatedPayload.offset,
-				validatedPayload.limit
+	.get('read_captures', readCapturesRpc.validator, readCapturesRpc.handler)
+	.post('create_capture', validator('json', createCaptureSchema), async (ctx) => {
+		const validatedPayload = ctx.req.valid('json');
+		const { type, storeId } = validatedPayload;
+		let { content } = validatedPayload;
+		let url: string | undefined;
+		if (type === 'url') {
+			url = content;
+			content = await scrape.regex(fetch, url);
+		}
+		const { embedding } = await rag.embed(content);
+		const chunkEmbeddings = await rag.embedChunks(content);
+		const payload = {
+			content,
+			embedding,
+			url,
+			type,
+			storeId
+		};
+		const result = await captureRepository.create(payload);
+		const createdCapture = result.first();
+		if (chunkEmbeddings.length > 0) {
+			await db.insert(captureChunkTable).values(
+				chunkEmbeddings.map((chunk) => ({
+					captureId: createdCapture.id,
+					content: chunk.content,
+					embedding: chunk.embedding
+				}))
 			);
-			const captures = result.all();
-			const formattedCaptures = captures.map(({ embedding, ...others }) => others);
-			return ctx.json(formattedCaptures);
 		}
-	)
-	.post(
-		'create_capture',
-		validator('json', createCaptureSchema),
-		async (ctx) => {
-			const validatedPayload = ctx.req.valid('json');
-			const { type, storeId } = validatedPayload;
-			let { content } = validatedPayload;
-			let url: string | undefined;
-			if (type === 'url') {
-				url = content;
-				content = await scrape.regex(fetch, url);
+		loggers.data.info('Created capture');
+		return ctx.json(createdCapture);
+	})
+	.post('create_captures', validator('json', createCapturesSchema), async (ctx) => {
+		const validatedPayload = ctx.req.valid('json');
+		const result = await captureRepository.createMany(validatedPayload.captures as Capture[]);
+		return ctx.json(result.first());
+	})
+	.patch('update_capture_enabled', validator('json', updateCaptureEnabledSchema), async (ctx) => {
+		const validatedPayload = ctx.req.valid('json');
+		await captureRepository.updateByPredicate(
+			validatedPayload.id,
+			eq(captureTable.storeId, validatedPayload.storeId),
+			{
+				enabled: validatedPayload.enabled
 			}
-			const { embedding } = await rag.embed(content);
-			const chunkEmbeddings = await rag.embedChunks(content);
-			const payload = {
-				content,
-				embedding,
-				url,
-				type,
-				storeId
-			};
-			const result = await captureRepository.create(payload);
-			const createdCapture = result.first();
-			if (chunkEmbeddings.length > 0) {
+		);
+		return ctx.json({ success: true });
+	})
+	.put('update_capture', validator('json', updateCaptureSchema), async (ctx) => {
+		const validatedPayload = ctx.req.valid('json');
+		const { content, enabled, id, type } = validatedPayload;
+		let embedding: number[] | undefined;
+		let url: string | undefined;
+		let textToEmbed: string = content;
+		let contentToStore: string = content;
+		let chunkEmbeddings: { content: string; embedding: number[] }[] | undefined;
+		let contentChanged = content !== validatedPayload.originalContent;
+
+		if (type === 'url') {
+			url = content;
+			const originalUrl = validatedPayload.originalUrl;
+			const urlChanged = originalUrl ? url !== originalUrl : contentChanged;
+			contentChanged = urlChanged;
+			if (urlChanged) {
+				textToEmbed = await scrape.regex(fetch, url);
+				contentToStore = textToEmbed;
+			} else {
+				contentToStore = validatedPayload.originalContent;
+				textToEmbed = contentToStore;
+			}
+		}
+
+		if (contentChanged) {
+			const embeddingResult = await rag.embed(textToEmbed);
+			embedding = embeddingResult.embedding;
+			chunkEmbeddings = await rag.embedChunks(textToEmbed);
+		}
+
+		const payload = {
+			embedding,
+			enabled,
+			content: contentToStore,
+			url
+		};
+		await captureRepository.updateByPredicate(
+			id,
+			eq(captureTable.storeId, validatedPayload.storeId),
+			payload
+		);
+		if (contentChanged) {
+			await db.delete(captureChunkTable).where(eq(captureChunkTable.captureId, id));
+			if (chunkEmbeddings && chunkEmbeddings.length > 0) {
 				await db.insert(captureChunkTable).values(
 					chunkEmbeddings.map((chunk) => ({
-						captureId: createdCapture.id,
+						captureId: id,
 						content: chunk.content,
 						embedding: chunk.embedding
 					}))
 				);
 			}
-			loggers.data.info('Created capture');
-			return ctx.json(createdCapture);
 		}
-	)
-	.post(
-		'create_captures',
-		validator('json', createCapturesSchema),
-		async (ctx) => {
-			const validatedPayload = ctx.req.valid('json');
-			const result = await captureRepository.createMany(validatedPayload.captures as Capture[]);
-			return ctx.json(result.first());
-		}
-	)
-	.patch(
-		'update_capture_enabled',
-		validator('json', updateCaptureEnabledSchema),
-		async (ctx) => {
-			const validatedPayload = ctx.req.valid('json');
-			await captureRepository.updateByPredicate(
-				validatedPayload.id,
+		loggers.data.info('Updated capture');
+		return ctx.json({ success: true });
+	})
+	.delete('delete_capture', validator('json', deleteCaptureSchema), async (ctx) => {
+		const validatedPayload = ctx.req.valid('json');
+		await captureRepository.deleteById(validatedPayload.id);
+		loggers.data.info('Deleted capture');
+		return ctx.body(null, 204);
+	})
+	.delete('delete_captures', validator('json', deleteCapturesSchema), async (ctx) => {
+		const validatedPayload = ctx.req.valid('json');
+		await captureRepository.deleteByPredicate(
+			and(
 				eq(captureTable.storeId, validatedPayload.storeId),
-				{
-					enabled: validatedPayload.enabled
-				}
-			);
-			return ctx.json({ success: true });
-		}
-	)
-	.put(
-		'update_capture',
-		validator('json', updateCaptureSchema),
-		async (ctx) => {
-			const validatedPayload = ctx.req.valid('json');
-			const { content, enabled, id, type } = validatedPayload;
-			let embedding: number[] | undefined;
-			let url: string | undefined;
-			let textToEmbed: string = content;
-			let contentToStore: string = content;
-			let chunkEmbeddings: { content: string; embedding: number[] }[] | undefined;
-			let contentChanged = content !== validatedPayload.originalContent;
-
-			if (type === 'url') {
-				url = content;
-				const originalUrl = validatedPayload.originalUrl;
-				const urlChanged = originalUrl ? url !== originalUrl : contentChanged;
-				contentChanged = urlChanged;
-				if (urlChanged) {
-					textToEmbed = await scrape.regex(fetch, url);
-					contentToStore = textToEmbed;
-				} else {
-					contentToStore = validatedPayload.originalContent;
-					textToEmbed = contentToStore;
-				}
-			}
-
-			if (contentChanged) {
-				const embeddingResult = await rag.embed(textToEmbed);
-				embedding = embeddingResult.embedding;
-				chunkEmbeddings = await rag.embedChunks(textToEmbed);
-			}
-
-			const payload = {
-				embedding,
-				enabled,
-				content: contentToStore,
-				url
-			};
-			await captureRepository.updateByPredicate(
-				id,
-				eq(captureTable.storeId, validatedPayload.storeId),
-				payload
-			);
-			if (contentChanged) {
-				await db.delete(captureChunkTable).where(eq(captureChunkTable.captureId, id));
-				if (chunkEmbeddings && chunkEmbeddings.length > 0) {
-					await db.insert(captureChunkTable).values(
-						chunkEmbeddings.map((chunk) => ({
-							captureId: id,
-							content: chunk.content,
-							embedding: chunk.embedding
-						}))
-					);
-				}
-			}
-			loggers.data.info('Updated capture');
-			return ctx.json({ success: true });
-		}
-	)
-	.delete(
-		'delete_capture',
-		validator('json', deleteCaptureSchema),
-		async (ctx) => {
-			const validatedPayload = ctx.req.valid('json');
-			await captureRepository.deleteById(validatedPayload.id);
-			loggers.data.info('Deleted capture');
-			return ctx.body(null, 204);
-		}
-	)
-	.delete(
-		'delete_captures',
-		validator('json', deleteCapturesSchema),
-		async (ctx) => {
-			const validatedPayload = ctx.req.valid('json');
-			await captureRepository.deleteByPredicate(
-				and(
-					eq(captureTable.storeId, validatedPayload.storeId),
-					inArray(captureTable.id, validatedPayload.captureIds)
-				)!!
-			);
-			return ctx.body(null, 204);
-		}
-	);
+				inArray(captureTable.id, validatedPayload.captureIds)
+			)!!
+		);
+		return ctx.body(null, 204);
+	});
