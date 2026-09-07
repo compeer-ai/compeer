79fd66774c9009a451b6e259314fc68231e3aefe
Liam Grossman <liam.j.grossman@gmail.com>
2026-09-07 12:25:05 -0400

    feat(memory): add necessary RPCs to operationlize

--- apps/memory/src/rpc/storeRpc.ts
+++ apps/memory/src/rpc/storeRpc.ts
@@ -1,27 +1,24 @@
-import { rpc as createRpc } from "@compeer-ai/rpc";
 import { Hono } from "hono";
 import * as v from "valibot";
-import type { Dependencies } from "../index";
+import type { Rpc } from "../index";
 
-const rpc = createRpc<Dependencies>((e) => {
-  console.error(e.message);
-});
+export function storeRpc(rpc: Rpc) {
+  const postStore = rpc.mutation(
+    "POST",
+    "/",
+    {
+      inputSchema: v.object({
+        name: v.string(),
+      }),
+      outputSchema: v.object({
+        name: v.string(),
+      }),
+    },
+    ({ name }, { database }) => {
+      database.connection(name);
+      return { name };
+    },
+  );
 
-export const postStore = rpc.mutation(
-  "POST",
-  "/",
-  {
-    inputSchema: v.object({
-      name: v.string(),
-    }),
-    outputSchema: v.object({
-      name: v.string(),
-    }),
-  },
-  ({ name }, { database }) => {
-    database.connection(name);
-    return { name };
-  },
-);
-
-export const storeRoute = new Hono().route("/", postStore.app);
+  return new Hono().route("/", postStore.app);
+}
