e9a15237f9d15b15db2cc78e01fb3abc882cd477
Liam Grossman <liam.j.grossman@gmail.com>
2026-09-11 21:15:09 -0400

    feat(atlas): delete index rpc (#306)
    * fix(memory): add name to memory rpc init
    
    * feat(atlas): create index rpc
    
    * feat(atlas): delete index rpc

--- apps/atlas/src/utilities/artifacts.ts
+++ apps/atlas/src/utilities/artifacts.ts
@@ -1,20 +1,23 @@
-import { mkdir, copyFile } from "node:fs/promises";
+import { mkdir, copyFile, unlink } from "node:fs/promises";
 import path from "node:path";
 import type { Config } from "..";
 
 export function artifacts(config: Pick<Config, "artifactsPath">) {
   async function create(file: string) {
     const source = path.resolve(process.cwd(), file);
-    const destination = path.join(
-      config.artifactsPath,
-      path.basename(source),
-    );
+    const destination = path.join(config.artifactsPath, path.basename(source));
     await mkdir(config.artifactsPath, { recursive: true });
     await copyFile(source, destination);
     return destination;
   }
 
+  async function _delete(artifact: string) {
+    const source = path.join(config.artifactsPath, artifact);
+    await unlink(source);
+  }
+
   return {
     create,
+    delete: _delete,
   };
 }
