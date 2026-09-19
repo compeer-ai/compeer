d6f7a871a0561369a043f809e19e74cffece30e8
Liam Grossman <liam.j.grossman@gmail.com>
2026-09-18 21:07:12 -0400

    feat(ui): create breadcrumb (#316)

---
+++ packages/ui/src/lib/components/Breadcrumbs.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { ChevronRight } from "@lucide/svelte";
+
+  interface Props {
+    crumbs: {
+      name: string;
+      href: string;
+    }[];
+  }
+
+  const { crumbs }: Props = $props();
+</script>
+
+<nav aria-label="Breadcrumb">
+  <ol class="flex items-center space-x-3">
+    {#each crumbs as { href, name }}
+      <li class="space-x-2 flex items-center">
+        <a {href} aria-label={name}>
+          {name}
+        </a>
+        <ChevronRight size={15} />
+      </li>
+    {/each}
+  </ol>
+</nav>
